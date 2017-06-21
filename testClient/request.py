c80326adb1255145863201914304c3720f2c851c
Maxwell-Zhang <zshcn1993@163.com>
2017-06-21 13:14:39 +0800

    testClient script

---
+++ testClient/request.py
@@ -0,0 +1,10 @@
+# coding: utf-8 
+
+import requests
+
+files = {"file":open("../01.jpg",'r')}
+url='111.231.110.120:5001/User/Login'
+r = requests.post(url,files=files)
+#print type(r)
+#print r
+print 'end'
