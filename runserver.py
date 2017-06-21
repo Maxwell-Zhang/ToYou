73f441ca91c4ae7870bc42e9c75a8e18e240aaaa
inocsin <vcheungyi@163.com>
2017-06-21 20:40:20 +0800

    Add models file Post.py User.py UserFavor.py, implement helper function UserHelper.py

--- runserver.py
+++ runserver.py
@@ -4,10 +4,10 @@ import sys
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from runtob import app
+from toyou import app
 
 if __name__ == '__main__':
-    port = 5000
+    port = 5008
     if len(sys.argv) == 2:
         port = int(sys.argv[1])
     httpServer = HTTPServer(WSGIContainer(app))
---
+++ setup_databese.py
@@ -0,0 +1,7 @@
+from toyou import app
+from toyou import db
+from toyou.models.User import User
+from toyou.models.Post import Post
+from toyou.models.UserFavor import UserFavor
+#db.drop_all()
+db.create_all()
