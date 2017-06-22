513decfcebb3ff0239d754a2257fb50f85399b15
Maxwell-Zhang <zshcn1993@163.com>
2017-06-23 00:18:27 +0800

    before merge update

--- testClient/request.py
+++ testClient/request.py
@@ -3,7 +3,7 @@
 import requests
 
 files = {"file":open("../01.jpg",'r')}
-url='111.231.110.120:5001/Zx/image_upload'
+url='http://111.231.110.120:5001/Zx/image_upload'
 r = requests.post(url,files=files)
 #print type(r)
 #print r
