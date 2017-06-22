e82375f93ea6995b115bc63af9fc74db8a57f5c2
inocsin <vcheungyi@163.com>
2017-06-23 00:36:05 +0800

    Add controller

---
+++ toyou/controller/GetMessageController.py
@@ -0,0 +1,100 @@
+#coding: utf-8
+import time
+import random
+import string
+from toyou  import app ,db
+#from runtob.models.User import User
+#from runtob.models.UserProfile import UserProfile
+#from runtob.models.UserLoginLog import UserLoginLog
+#from runtob.models.UserFightPower import UserFightPower
+#from runtob.common import permission
+from flask import Blueprint, request, jsonify
+
+bp = Blueprint('get_message', __name__)
+
+
+@bp.route('/Get_message', methods=['GET'])
+def getmessage():
+    AccountId = request.args.get("account")
+    MessageId = request.args.get("message_id")
+    print str(AccountId)
+    print str(MessageId)
+    return str(AccountId)+str(MessageId)
+    '''
+    userId = request.form['userId']
+    token = request.form['token']
+    loginDateTime = request.form['loginDateTime']
+    loginLocationX = request.form['loginLocationX']
+    loginLocationY = request.form['loginLocationY']
+    user = User.query.filter_by(id=userId).first()
+    if user is None:
+        user = User.query.filter_by(username=userId).first()
+        if user is None:
+            user = User(username=userId, token='', lastLogin=loginDateTime)
+            r.set('user_id_' + str(user.id), token)
+            db.session.add(user)
+            db.session.commit()
+            db.session.add(UserProfile(user.id))
+            db.session.commit()
+            db.session.add(UserFightPower(user.id))
+            db.session.commit()
+    else:
+        rToken = r.get('user_id_' + str(userId))
+        if token != rToken:
+            return jsonify(
+                {'state': 401, 'error': 'token error', 'data': {'loginState': 1, 'token': '', 'userId': user.id}})
+    user.token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
+    r.set('user_id_' + str(user.id), user.token, ex=10000000000)
+    db.session.commit()
+    db.session.add(UserLoginLog(user.id, loginDateTime, loginLocationX, loginLocationY))
+    db.session.commit()
+    return jsonify({'state': 200, 'error': '', 'data': {'loginState': 0, 'token': user.token, 'userId': user.id}}) '''
+
+'''
+@bp.route('/UpdateInformation', methods=['POST'])
+#@permission
+def UpdateInformation():
+    userId = request.form['userId']
+    gender = request.form['gender']
+    nickname = request.form['nickname']
+    phone = request.form['phone']
+    email = request.form['email']
+    avatar = request.form['avatar']
+    user = UserProfile.query.get(userId)
+    user.gender = {
+            '0':'male',
+            '1':'female',
+        }.get(gender, '')
+    user.phone = phone
+    user.nickname = nickname
+    user.email = email #todo 校验
+    user.avatar = avatar
+    db.session.commit()
+    return jsonify({'state': 200, 'error': ''})
+
+
+@bp.route('/GetInformation', methods=['POST'])
+#@permission
+def GetInformation():
+    queryId = request.form['queryId']
+    user = User.query.get(queryId)
+    profile = UserProfile.query.get(queryId)
+    if profile is None:
+        return jsonify({'state': 404, 'error': 'invalid query userid'})
+    data = {
+        'nickname': profile.nickname,
+        'gender': {
+            'male': 0,
+            'female': 1,
+        }.get(profile.gender, 2),
+        'phone': profile.phone,
+        'email': profile.email,
+        'lastLoginDateTime': time.mktime(time.strptime(str(user.lastLogin), "%Y-%m-%d %H:%M:%S")),
+        'money': user.money,
+        'avatar': profile.avatar
+
+    }
+    return jsonify({'state': 200, 'error': '', 'data': data})
+'''
+
+app.register_blueprint(bp, url_prefix='/GetMessage')
---
+++ toyou/controller/ImageUploadController.py
@@ -0,0 +1,60 @@
+#coding:utf-8
+import string
+import os
+#import sys
+#sys.path.append('toyou/helpers/')
+#import Image
+from toyou import app,db
+from toyou.helpers.UserHelper import addPostByName
+from toyou.models.Post import Post
+from toyou.models.User import User
+from toyou.models.UserFavor import UserFavor
+from flask import Flask,redirect,url_for,Blueprint,request,jsonify
+from werkzeug import secure_filename
+UPLOAD_FOLDER = './pic'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+else:
+    pass
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+bp = Blueprint('Zx',__name__)
+@bp.route('/image_upload',methods=['POST'])
+def uploadImage():
+	image_files = request.files.getlist('image[]')
+	image_URL = []
+        for file in image_files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                image_URL.append(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	#tag_id = getAITagId(image_files)
+        tag_id = 1
+	return jsonify(result='true',image_URL=image_URL,tag_id=tag_id)
+
+	
+@bp.route('/new_message_upload',methods=['POST'])
+def uploadMessage():
+	qq =  request.form.get('account')
+	content = request.form.get('message')
+	imagelist = request.form.getlist('image_URL')
+	tag = request.form.getlist('user_tag_id')
+        user = User.query.filter_by(qq=qq).first()
+	addPostByname(user.username,content,tag,imagelist)
+        return jsonify(result='true')
+
+@bp.route('/own_message',methods=['POST'])
+def getOwnMessage():
+	qq =  request.form.get('account')
+	user = User.query.filter_by(qq=qq).first()
+	own_message = Post.query.filter_by(userid=user.id).all()
+	messages_ids = []
+	for u in own_message:
+		message_ids.append(u.id)
+	return jsonify(result = 'true',message_ids=message_ids);
+app.register_blueprint(bp,url_prefix='/Zx')
